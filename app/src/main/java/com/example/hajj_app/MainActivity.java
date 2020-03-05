4ea7f592f951e7976bb0297d9d3c5ab854dcb61b
Abdulnour Dualeh <abdulnourdualeh@gmail.com>
2020-03-05 14:56:21 +0000

    added facebook sdk
    Signed-off-by: Abdulnour Dualeh <abdulnourdualeh@gmail.com>

--- app/src/main/java/com/example/hajj_app/MainActivity.java
+++ app/src/main/java/com/example/hajj_app/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 public class MainActivity extends AppCompatActivity {
 
